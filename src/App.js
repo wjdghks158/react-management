889b60c137c4551dd48aae20b73fe52adec295b1
wjdghks158 <wjdghks158@naver.com>
2020-03-31 20:01:18 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
 
+
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/any',
+    'name' : '홍길동',
+    'birthday' : '961222',
+    'gender' : '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/any',
+    'name' : '까오르',
+    'birthday' : '961222',
+    'gender' : '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/any',
+    'name' : '김끼끼',
+    'birthday' : '961222',
+    'gender' : '남자',
+    'job': '대학생'
+  }
+]
 class App extends Component {
   render() {
-    return (
-      <div className="gary-background">
-        <img src={logo} lat="logo"/>
-        <h2>Let's develop management system</h2>
+    return(
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+            );
+          })
+        }
       </div>
     );
+
   }
 }
 
